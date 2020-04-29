de36b122519f1428b5beb75ca66ede0e9295944c
nikhilrathor <nikhilrathor01@gmail.com>
2020-04-29 18:22:48 +0530

    Express API and MongoDB

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const db = require('./config/keys').mongoURI;
+const items = require('./routes/api/items');
+
+const app = express();
+
+app.use(express.json());
+
+mongoose.connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(() => console.log("MongoDB Connected"))
+    .catch(err => console.log(err));
+
+app.use('/api/items',items);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
