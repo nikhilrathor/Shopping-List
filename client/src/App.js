6b96a75fbdbc8ead23806e2a6a0285e6c2872d41
nikhilrathor <nikhilrathor01@gmail.com>
2020-04-30 19:55:11 +0530

    Redux Auth state

--- client/src/App.js
+++ client/src/App.js
@@ -6,11 +6,17 @@ import AppNavbar from './components/AppNavbar';
 import ShoppingList from './components/ShoppingList';
 import ItemModal from './components/ItemModal';
 import { Container } from 'reactstrap'
+import { loadUser } from './actions/authActions';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 class App extends Component {
+
+  componentDidMount(){
+    store.dispatch(loadUser());
+  }
+
   render() {
     return (
       <Provider store={store}>
